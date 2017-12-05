f67c7d9f78bb0d71e4a1b5ec53b80ff3df08fbc7
emai1 <emai1@stuy.edu>
2017-12-05 18:52:38 -0500

    Added new class Bowman

---
+++ v2/Bowman.java
@@ -0,0 +1,16 @@
+public class Bowman extends Protagonist {
+	
+	public Bowman(String name) {
+		super(name);
+		health -= 10;
+		strength += 10;
+		defense -= 5;
+		attack += 0.2;
+	}
+	
+	public String toString() {
+		return "Class: Bowman" 
+			 + System.lineSeparator()
+			 + super.toString();
+	}
+}
