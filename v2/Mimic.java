c4fddf5e5b06f9902a5e01eddb1bb797ee246b38
LeoGreenleaf <34113324+LeoGreenleaf@users.noreply.github.com>
2017-12-05 23:13:15 -0500

    Create Mimic.java
    Added Mimic enemy (fake chest thing)

---
+++ v2/Mimic.java
@@ -0,0 +1,14 @@
+public class Mimic extends Monster{
+    
+    public Mimic() {
+        super();
+        health -= 50
+        strength += 10
+        defense += 20
+        attack += 0.5
+    }
+
+    public String  toString() {
+        return "Monster: Mimic" + super.toString();
+    } 
+}
