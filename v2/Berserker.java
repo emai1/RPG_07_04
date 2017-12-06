a3523c6a5f6c915344558e5b047e38219acd15e9
LeoGreenleaf <34113324+LeoGreenleaf@users.noreply.github.com>
2017-12-05 23:06:49 -0500

    Create Berserker.java
    Berserker Class

---
+++ v2/Berserker.java
@@ -0,0 +1,14 @@
+public class Berserker extends Protagonist {
+
+    public Berserker(String name) {
+        super(name);
+        health -= 50
+        strength += 25
+        attack += 3.6
+        defense -= 2-
+    }
+
+    public String  toString() {
+        return "Class: Berserker" + System.lineSeperator() + super.toString();
+    } 
+}
