17e1a7d5497f114fc6d3b22d1e7426c891ec4f6a
emai1 <emai1@stuy.edu>
2017-12-06 20:01:39 -0500

    Reformmated toString

--- v2/Mage.java
+++ v2/Mage.java
@@ -1,13 +1,14 @@
 public class Mage extends Protagonist {
 
     public Mage(String newName) {
-        super(name);
-        health += 10
-        attack += 1.5
+        super(newName);
+        health += 10;
+        attack += 1.5;
     }
 
-    public String  toString() {
-        super.toString()
-        + System.lineSeparator() "This protagonist is a mage.";
+    public String toString() {
+        return "Class: Mage"
+			 + System.lineSeparator()
+			 + super.toString();
     } 
 }
