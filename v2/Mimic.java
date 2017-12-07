0201414bc01b5eb226bce6ca0c350afd0ba48105
emai1 <emai1@stuy.edu>
2017-12-06 20:01:59 -0500

    Reformmated toString

--- v2/Mimic.java
+++ v2/Mimic.java
@@ -2,13 +2,14 @@ public class Mimic extends Monster{
     
     public Mimic() {
         super();
-        health -= 50
-        strength += 10
-        defense += 20
-        attack += 0.5
+        health -= 50;
+        strength += 10;
+        defense += 20;
+        attack += 0.5;
     }
 
     public String toString() {
-        return "Monster: Mimic" + super.toString();
+        return "Mimic / "
+			 + super.toString();
     } 
 }
