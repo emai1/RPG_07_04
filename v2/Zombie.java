e356495067fbcab1fac6ecd6f45b7eeca91a7657
emai1 <emai1@stuy.edu>
2017-12-06 20:02:09 -0500

    Reformmated toString

--- v2/Zombie.java
+++ v2/Zombie.java
@@ -2,12 +2,12 @@ public class Zombie extends Monster{
     
     public Zombie() {
         super();
-        defense += 20
-        health += 5
+        defense += 20;
+        health += 5;
     }
 
-    public String  toString() {
-        super.toString()
-        + System.lineSeparator() "This monster is a zombie.";
+    public String toString() {
+        return "Zombie / "
+			 + super.toString();
     } 
 }
